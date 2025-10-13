7af2763910b08ddfcf605bb644717cb08e9374b3
Kenji Saito <ken-yo@mbr.nifty.com>
2025-10-13 22:49:22 +0900

    Bumps node modules

--- non-react/eslint.config.ts
+++ non-react/eslint.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'eslint/config';
 import eslint from '@eslint/js';
+import { ConfigObject } from '@eslint/core';
 import { configs, parser } from 'typescript-eslint';
 import stylistic from '@stylistic/eslint-plugin';
 import importPlugin from 'eslint-plugin-import';
@@ -14,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const gitignorePath = path.resolve(__dirname, '../.gitignore');
 
-const eslintConfig = defineConfig(
+const eslintConfig: ConfigObject[] = defineConfig(
   {
     ignores: [
       ...(includeIgnoreFile(gitignorePath).ignores || []),
--- react-based/nextjs-based/eslint.config.ts
+++ react-based/nextjs-based/eslint.config.ts
@@ -1,25 +1,18 @@
+// @ts-check
+
 import { defineConfig } from 'eslint/config';
 import eslint from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
 import react from 'eslint-plugin-react';
-import globals from 'globals';
-
-import nextPlugin from '@next/eslint-plugin-next';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
-
-// @ts-expect-error ignore type error --- IGNORE ---
-import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
-
-// @ts-expect-error ignore type error --- IGNORE ---
-import flowtypePlugin from 'eslint-plugin-flowtype';
-
-// @ts-expect-error ignore type error --- IGNORE ---
+import reactHooks from 'eslint-plugin-react-hooks';
+// import jsxA11y from 'eslint-plugin-jsx-a11y';
+// import importPlugin from 'eslint-plugin-import';
+// @ts-expect-error ignore plugin type
 import pluginPromise from 'eslint-plugin-promise';
-
+// import reactRefresh from "eslint-plugin-react-refresh";
+import globals from 'globals';
 import { configs, parser } from 'typescript-eslint';
 import { FlatCompat } from '@eslint/eslintrc';
-
-const compat = new FlatCompat();
 
 import { includeIgnoreFile } from '@eslint/compat';
 import path from 'node:path';
@@ -27,50 +20,54 @@ import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, '.gitignore');
+const gitignorePath = path.resolve(__dirname, './.gitignore');
 
-const nextCompat = compat.config(
-  {
-    plugins: nextPlugin.configs.recommended.plugins,
-  },
-);
+const compat = new FlatCompat();
 
-const eslintConfig =  defineConfig(
+export default defineConfig(
   includeIgnoreFile(gitignorePath),
   {
     ignores: [
-      '*.d.ts',
+      '**/*.d.ts',
+      '*.js',
       'src/tsconfig.json',
-      '*.css',
+      'src/next-env.d.ts',
+      'src/stories',
       'node_modules/**/*',
-      '.next',
-      'out',
+      './.next/*',
     ],
   },
   eslint.configs.recommended,
-  configs.strict,
-  configs.stylistic,
-  ...compat.config({
-    extends: ['next/core-web-vitals'],
-  }),
-  ...nextCompat,
+  ...configs.strict,
+  ...configs.stylistic,
   pluginPromise.configs['flat/recommended'],
-  {
-    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
-    languageOptions: {
-      parser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-        tsconfigRootDir: import.meta.dirname,
+  reactHooks.configs.flat.recommended,
+  // reactRefresh.configs.recommended,
+  // jsxA11y.flatConfigs.recommended,
+  ...compat.config({
+    extends: ['next'],
+    settings: {
+      next: {
+        rootDir: '.',
       },
+    },
+  }),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    ...react.configs.flat.recommended,
+    ...react.configs.flat['jsx-runtime'],
+    languageOptions: {
+      ...react.configs.flat.recommended.languageOptions,
+      parser,
       globals: {
+        ...globals.serviceworker,
         ...globals.browser,
       },
     },
+    // extends: [
+    //   importPlugin.flatConfigs.recommended,
+    //   importPlugin.flatConfigs.typescript,
+    // ],
     settings: {
       react: {
         version: 'detect',
@@ -82,49 +79,23 @@ const eslintConfig =  defineConfig(
       ],
       'import/internal-regex': '^~/',
       'import/resolver': {
-        node: true,
-        typescript: true,
+        node: {
+          extensions: ['.ts', '.tsx'],
+        },
+        typescript: {
+          alwaysTryTypes: true,
+        },
       },
     },
     plugins: {
       '@stylistic': stylistic,
-      '@stylistic/ts': stylistic,
-      '@stylistic/js': stylistic,
-      '@stylistic/jsx': stylistic,
-      react,
-      'jsx-a11y': jsxA11yPlugin,
-      // '@next/next': nextPlugin,
-      'flow-type': flowtypePlugin,
-      'react-hooks': reactHooksPlugin,
     },
-    extends: [
-      ...compat.config(jsxA11yPlugin.configs.recommended),
-    ],
-    // @ts-expect-error ignore type error
     rules: {
-      'react/jsx-uses-react': 'off',
-      'react/jsx-uses-vars': 'off',
-      'react/require-render-return': 'off',
-      'react/display-name': 'off',
-      'react/no-direct-mutation-state': 'off',
-      'react/no-string-refs': 'off',
-      'react/jsx-no-undef': 'off',
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs['core-web-vitals'].rules,
-      ...reactHooksPlugin.configs.recommended.rules,
-      '@next/next/no-duplicate-head': 'off',
-      '@next/next/no-img-element': 'error',
-      '@next/next/no-page-custom-font': 'off',
-      // 'import/namespace': 'off',
-      // 'import/no-named-as-default': 'off',
-      // 'import/no-named-as-default-member': 'off',
+      '@stylistic/semi': 'error',
       '@stylistic/indent': ['error', 2],
-      '@stylistic/semi': ['error', 'always'],
       '@stylistic/comma-dangle': ['error', 'always-multiline'],
       '@stylistic/arrow-parens': ['error', 'always'],
       '@stylistic/quotes': ['error', 'single'],
     },
   },
 );
-
-export default eslintConfig;
