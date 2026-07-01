d99d5d63f1cbca2e46828101b7ed1b99e742f79d
Kenji Saito <ken-yo@mbr.nifty.com>
2026-07-01 23:44:07 +0900

    Bumps node modules

--- non-react/eslint.config.ts
+++ non-react/eslint.config.ts
@@ -11,7 +11,7 @@ import pluginPromise from 'eslint-plugin-promise';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const gitignorePath = path.resolve(__dirname, '../.gitignore');
+const gitignorePath = path.resolve(__dirname, '.gitignore');
 
 export default defineConfig(
   includeIgnoreFile(gitignorePath),
